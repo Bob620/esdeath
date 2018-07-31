36e5efe6115e53d45cdb5daed50e89daa586f787
bob620 <bruder.kraft225@gmail.com>
2018-07-31 18:31:22 -0500

    feat(index) made the index page do things

---
+++ index.js
@@ -0,0 +1,5 @@
+const feedManager = require('./components/feedmanager');
+console.info('[General] Feed Service Loaded');
+
+const discordManager = require('./components/discordmanager');
+console.info('[General] Discord Service Loaded');
